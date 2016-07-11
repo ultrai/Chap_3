264a4b0a4d36d21a80c912376559b5fcf6b03170
ultrai <phani.moresmiles@gmail.com>
2016-07-11 13:27:33 +0530

    Add files via upload

---
+++ prepare_model.lua
@@ -0,0 +1,30 @@
+require 'cunn'
+require 'cudnn'
+
+local pl = require('pl.import_into')()
+
+local googlenet = dofile('googlenet.lua')
+local net = googlenet({
+  cudnn.SpatialConvolution,
+  cudnn.SpatialMaxPooling,
+  cudnn.ReLU,
+  cudnn.SpatialCrossMapLRN
+})
+--net:cuda()
+net:remove(24)
+net:remove(24)
+net:remove(24)
+torch.save('../inception.t7',net)
+local googlenet = dofile('googlenet2.lua')
+local net = googlenet({
+  cudnn.SpatialConvolution,
+  cudnn.SpatialMaxPooling,
+  cudnn.ReLU,
+  cudnn.SpatialCrossMapLRN
+})
+--net:cuda()
+net:remove(24)
+net:remove(24)
+net:remove(24)
+torch.save('../inception2.t7',net)
+
